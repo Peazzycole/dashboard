59ca5d67d8bb5aa5646b4b41e9b5c8c3ad332722
Peace Oghenevwefe <Peazzycole@gmail.com>
2023-11-21 10:39:52 +0100

    Finished Dashboard implementation

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from "../../ui/skeletons"
+
+// The (Overview) folder is to make set the pages where the skeleton will be shown
+
+export default function Loading() {
+    return <DashboardSkeleton />
+}
